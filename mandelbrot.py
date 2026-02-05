8de3b753df4306e6f28137dd8c274a85e4a8f405
Nusrat Jahan Raufi <nusratjahan2@gmail.com>
2026-02-05 14:18:13 +0100

    Add mandelbrot skeleton

---
+++ mandelbrot.py
@@ -0,0 +1,24 @@
+"""
+Mandelbrot Set Generator
+Author: Nusrat Jahan Raufi
+Course: Numerical Scientific Computing 2026
+"""
+
+def mandelbrot_point(c, max_iter=100):
+    """
+    Compute Mandelbrot iteration count for a single complex number.
+
+    Parameters
+    ----------
+    c : complex
+        Complex point
+    max_iter : int
+        Maximum number of iterations
+
+    Returns
+    -------
+    int
+        Iteration count
+    """
+    # TODO: Implement the algorithm
+    pass
